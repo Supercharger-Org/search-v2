cde04a19ed09f119cc6da243e74a1fdf8bf0ad59
fjrsc <franz@supercharge.work>
2025-02-05 12:42:22 -0700

    Create eventTypes.js

---
+++ main-config/eventTypes.js
@@ -0,0 +1,17 @@
+// eventTypes.js
+export const EventTypes = {
+  LOAD_SESSION: "session:load",
+  LIBRARY_SELECTED: "library:selected",
+  METHOD_SELECTED: "method:selected",
+  DESCRIPTION_UPDATED: "description:updated",
+  DESCRIPTION_IMPROVED: "description:improved", // Changed from DESCRIPTION_VALIDATED
+  FILTER_ADDED: "filter:added",
+  FILTER_UPDATED: "filter:updated",
+  PATENT_SEARCH_INITIATED: "patent:search:initiated",
+  PATENT_INFO_RECEIVED: "patent:info:received",
+  KEYWORDS_STEP_ADDED: "keywords:step:added",
+  KEYWORDS_GENERATE_INITIATED: "keywords:generate:initiated",
+  KEYWORDS_GENERATE_COMPLETED: "keywords:generate:completed",
+  KEYWORD_REMOVED: "keyword:removed",
+  KEYWORD_ADDED: "keyword:added"
+};
