95fa11e93ffb598b0e8e19b295163db2d1e2dc6d
fjrsc <franz@supercharge.work>
2025-02-05 12:48:20 -0700

    Create logger.js

---
+++ main-config/logger.js
@@ -0,0 +1,20 @@
+// logger.js
+export const Logger = {
+  enabled: true, // Toggle logging globally
+
+  log: (...args) => {
+    if (Logger.enabled) console.log(...args);
+  },
+
+  error: (...args) => {
+    if (Logger.enabled) console.error(...args);
+  },
+
+  warn: (...args) => {
+    if (Logger.enabled) console.warn(...args);
+  },
+
+  info: (...args) => {
+    if (Logger.enabled) console.info(...args);
+  }
+};
