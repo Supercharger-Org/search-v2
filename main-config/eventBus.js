fb33cab0af6a4763172053d460d001f17e573537
fjrsc <franz@supercharge.work>
2025-02-14 12:43:05 -0700

    Update eventBus.js

--- main-config/eventBus.js
+++ main-config/eventBus.js
@@ -1,4 +1,3 @@
-// eventBus.js
 import { Logger } from './logger.js';
 
 export default class EventBus {
@@ -6,7 +5,6 @@ export default class EventBus {
     this.listeners = new Map();
   }
 
-  // Register a listener for a specific event type
   on(eventType, callback) {
     if (!this.listeners.has(eventType)) {
       this.listeners.set(eventType, new Set());
@@ -15,7 +13,16 @@ export default class EventBus {
     Logger.log(`Listener added for event: ${eventType}`);
   }
 
-  // Emit an event with data payload
+  off(eventType, callback) {
+    const listeners = this.listeners.get(eventType);
+    if (listeners) {
+      listeners.delete(callback);
+      if (listeners.size === 0) {
+        this.listeners.delete(eventType);
+      }
+    }
+  }
+
   emit(eventType, data) {
     const listeners = this.listeners.get(eventType);
     if (listeners) {
